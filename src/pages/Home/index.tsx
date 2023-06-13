ca3b8d6c6d1aa97b0b577211cc2d8fa11b9aa822
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-13 17:35:44 -0300

    importando o componente Sidebar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,63 +1,29 @@
-import logoImage from '../../assets/logo.svg'
-import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from 'phosphor-react'
-import './styles.css'
-import { Tweet } from '../../components/Tweet'
+import { Sparkle } from "phosphor-react";
+import "./styles.css";
+import { Tweet } from "../../components/Tweet";
+import { Sidebar } from "../../components/Sidebar";
 
 export function Home() {
   return (
     <div className="layout">
-      <aside className="sidebar">
-        <img className='logo' src={logoImage} alt="Logo do Twitter" />
-        <nav className='main-navigation'>
-          <a className='active' href=''>
-            <House weight='fill' />
-            Home
-          </a>
-          <a href=''>
-            <Hash />
-            Explore
-          </a>
-          <a href=''>
-            <Bell />
-            Notification
-          </a>
-          <a href=''>
-            <Envelope />
-            Messages
-          </a>
-          <a href=''>
-            <BookmarkSimple />
-            Bookmarks
-          </a>
-          <a href=''>
-            <FileText />
-            Lists
-          </a>
-          <a href=''>
-            <User />
-            Profile
-          </a>
-          <a href=''>
-            <DotsThreeCircle />
-            More
-          </a>
-        </nav>
-        <button className='new-tweet' type='button'>Tweet</button>
-      </aside>
+      <Sidebar />
       <div className="content">
-        <main className='timeline'>
-          <div className='timeline-header'>
+        <main className="timeline">
+          <div className="timeline-header">
             Home
             <Sparkle />
           </div>
-          <form className='new-tweet-form'>
-            <label htmlFor='tweet'>
-              <img src='https://github.com/RayanneRamos.png' alt='Rayanne Ramos' />
-              <textarea id='tweet' placeholder="What's happening?" />
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img
+                src="https://github.com/RayanneRamos.png"
+                alt="Rayanne Ramos"
+              />
+              <textarea id="tweet" placeholder="What's happening?" />
             </label>
-            <button type='submit'>Tweet</button>
+            <button type="submit">Tweet</button>
           </form>
-          <div className='separator' />
+          <div className="separator" />
           <Tweet />
           <Tweet />
           <Tweet />
@@ -65,5 +31,5 @@ export function Home() {
         </main>
       </div>
     </div>
-  )
+  );
 }
