84344d1b98aa83235e02d3bccf6e84abbf85f285
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-13 16:53:53 -0300

    criando a sidebar e a sua estilização

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,42 @@ export function Home() {
   return (
     <div className="layout">
       <aside className="sidebar">
-        
+      <img className='logo' src={logoImage} alt="Logo do Twitter" />
+        <nav className='main-navigation'>
+          <a className='active' href=''>
+            <House weight='fill'/>
+            Home
+          </a>
+          <a href=''>
+            <Hash />
+            Explore
+          </a>
+          <a href=''>
+            <Bell />
+            Notification
+          </a>
+          <a href=''>
+            <Envelope />
+            Messages
+          </a>
+          <a href=''>
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href=''>
+            <FileText />
+            Lists
+          </a>
+          <a href=''>
+            <User />
+            Profile
+          </a>
+          <a href=''>
+            <DotsThreeCircle />
+            More
+          </a>
+        </nav>
+        <button className='new-tweet' type='button'>Tweet</button>
       </aside>
       <div className="content">
         
