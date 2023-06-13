dfc3b1f73bd9b7042e3578218d1acf1fd20b2b25
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-13 17:40:17 -0300

    importando o componente Header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
-import { Sparkle } from "phosphor-react";
-import "./styles.css";
 import { Tweet } from "../../components/Tweet";
 import { Sidebar } from "../../components/Sidebar";
+import "./styles.css";
+import { Header } from "../../components/Header";
 
 export function Home() {
   return (
@@ -9,10 +9,7 @@ export function Home() {
       <Sidebar />
       <div className="content">
         <main className="timeline">
-          <div className="timeline-header">
-            Home
-            <Sparkle />
-          </div>
+          <Header title="Home" />
           <form className="new-tweet-form">
             <label htmlFor="tweet">
               <img
