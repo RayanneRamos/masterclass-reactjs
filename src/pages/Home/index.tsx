08ddb56f144156dc46731ece23b8b75ba856dbab
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-13 16:52:56 -0300

    criando a página Home e a sua estilização

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import logoImage from '../../assets/logo.svg'
+import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from 'phosphor-react'
+import './styles.css'
+
+export function Home() {
+  return (
+    <div className="layout">
+      <aside className="sidebar">
+        
+      </aside>
+      <div className="content">
+        
+      </div>
+    </div>
+  )
+}
