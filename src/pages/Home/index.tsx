4c961ef8fbdf97f201b54f5589649652b1613648
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-14 15:31:50 -0300

    importando o arquivo de rotas

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,14 @@
-import { Tweet } from "../../components/Tweet";
 import { Sidebar } from "../../components/Sidebar";
 import "./styles.css";
-import { Header } from "../../components/Header";
-import { Separator } from "../../components/Separator";
+import { RouterProvider } from "react-router-dom";
+import { router } from "../../routes";
 
 export function Home() {
   return (
     <div className="layout">
       <Sidebar />
       <div className="content">
-        <main className="timeline">
-          <Header title="Home" />
-          <form className="new-tweet-form">
-            <label htmlFor="tweet">
-              <img
-                src="https://github.com/RayanneRamos.png"
-                alt="Rayanne Ramos"
-              />
-              <textarea id="tweet" placeholder="What's happening?" />
-            </label>
-            <button type="submit">Tweet</button>
-          </form>
-          <Separator />
-          <Tweet />
-          <Tweet />
-          <Tweet />
-          <Tweet />
-        </main>
+        <RouterProvider router={router} />
       </div>
     </div>
   );
