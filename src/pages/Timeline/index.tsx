3fd7f51e816882de984c7c8336393452b189fa0f
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-06-14 16:52:01 -0300

    criando um novo tweet

--- src/pages/Timeline/index.tsx
+++ src/pages/Timeline/index.tsx
@@ -1,18 +1,36 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../../components/Header";
 import { Separator } from "../../components/Separator";
 import { Tweet } from "../../components/Tweet";
 import "./styles.css";
 
-const tweets = ["Meu primeiro tweet", "Teste", "Deu certo"];
-
 export function Timeline() {
+  const [newTweet, setNewTweet] = useState("");
+  const [tweets, setTweets] = useState([
+    "Meu primeiro tweet",
+    "Teste",
+    "Deu certo",
+  ]);
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault();
+
+    setTweets([newTweet, ...tweets]);
+    setNewTweet("");
+  }
+
   return (
     <main className="timeline">
       <Header title="Home" />
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/RayanneRamos.png" alt="Rayanne Ramos" />
-          <textarea id="tweet" placeholder="What's happening?" />
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            value={newTweet}
+            onChange={(event) => setNewTweet(event.target.value)}
+          />
         </label>
         <button type="submit">Tweet</button>
       </form>
